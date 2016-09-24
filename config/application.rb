cc888c15ac7db34d132ec1577115ff163495af25
Alberto Vena <kennyadsl@gmail.com>
2016-09-24 12:44:05 +0300

    Add Rack Cors middleware

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,10 @@ module Blog
     # -- all .rb files in that directory are automatically loaded.
 
     config.middleware.insert_before 0, "Rack::Cors" do
-     allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
-     end
-   end
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
