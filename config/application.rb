a7132007840c7a4bd3b1e405e24789360c2cadbf
Alberto Vena <kennyadsl@gmail.com>
2016-09-24 12:52:19 +0300

    Autoload graph files by default

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module GraphQLBlog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths << Rails.root.join('app', 'graph', 'types')
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
