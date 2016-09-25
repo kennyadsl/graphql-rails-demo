efc43c2f1c4d0fd0a542f0cf899c34f318c6edb3
Alberto Vena <kennyadsl@gmail.com>
2016-09-25 10:53:54 +0300

    Emtpy README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+luke = Author.create(name: 'Luke Skywalker')
+
+Blog.create(
+  title: 'Building a bot',
+  content: 'How to build a bot day-to-day report',
+  author: luke
+)
